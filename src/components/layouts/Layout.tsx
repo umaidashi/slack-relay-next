5222d48100ca2bce1890a49e9d5d6ed10ff313e0
umaidashi <umaidashi18@gmail.com>
2023-03-04 09:47:28 +0900

    超簡単認証ついか

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/components/layouts/Layout.module.scss";
 import Sidebar from "./Sidebar";
 import { DataType } from "../pages/Page";
 
+const PASS = process.env.PASS;
+
 export default function Layout(props: any) {
+  const [auth, setAuth] = useState<boolean>(false);
   const data: DataType = props.data;
+
+  useEffect(() => {
+    const result = window.prompt("password");
+    if (result === PASS) {
+      setAuth(true);
+    } else {
+      alert("パスワードが違います。");
+    }
+  }, []);
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>Slack log viewer ver.2</div>
-      <div className={styles.body}>
-        <Sidebar data={data} />
-        {props.children}
-      </div>
-    </div>
+    <>
+      {auth ? (
+        <div className={styles.container}>
+          <>
+            <div className={styles.header}>Slack log viewer ver.2</div>
+            <div className={styles.body}>
+              <Sidebar data={data} />
+              {props.children}
+            </div>
+          </>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 }
